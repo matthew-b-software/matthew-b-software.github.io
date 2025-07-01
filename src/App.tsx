9ae933ce945b622c118e5a878cdacad98ceae3e4
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-01 15:24:41 -0700

    setup basic colors and root

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 /** import { useState } from 'react' */
 import './App.css'
+import ContactInfo from './components/ContactInfo'
 
 function App() {
 
   return (
     <>
       <h1> website development is in progress </h1>
+      <ContactInfo />
     </>
   )
 }
