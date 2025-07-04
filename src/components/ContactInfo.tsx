26da6d9661b9962f54e82bb1befddb19a72ab4ea
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-04 16:51:50 -0700

    Got links rendering from array, logo path not working yet.

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,8 +1,37 @@
+import Link from './Link'
+
+const links = [
+    {
+        link: "https://www.linkedin.com/in/matthew-beitler/",
+        link_text: "LinkedIn",
+        logo_scr: "../assets/linkedin-logo.png",
+        logo_alt: "LinkedIn Logo",
+    },
+    {
+        link: "https://www.linkedin.com/in/matthew-beitler/",
+        link_text: "LinkedIn",
+        logo_scr: "../assets/linkedin-logo.png",
+        logo_alt: "LinkedIn Logo",
+    }
+]
+
 /** This component shall contain my contact info (links, email, & phone) */
 function ContactInfo() {
     return (
         <>
-            <h1>HI</h1>
+            <h1>Contact Me</h1>
+            <ul>
+                {links.map((item, index) => (
+                <li key={index}>
+                <Link 
+                    link={item.link}
+                    link_text={item.link_text}
+                    logo_scr={item.logo_scr}
+                    logo_alt={item.logo_alt}
+                    />
+                </li>
+                ))}  
+            </ul>
         </>
     )
 }
