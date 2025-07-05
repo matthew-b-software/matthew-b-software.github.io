7b10ed521fd0a083a53bfebf71a74690a961dc2e
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-05 15:17:52 -0700

    Removed unnecessary <>

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -10,12 +10,10 @@ interface Props {
 /** This component shall contain a link and image to be used in "ContactInfo" */
 function Link({ link, link_text, logo_src, logo_alt }: Props) {
     return (
-        <>
-            <a href={link} target="_blank" rel={"Visit my " + link_text} className='SocialLink'>
-                <img src={logo_src} alt={logo_alt} />
-                {link_text}
-            </a>
-        </>
+        <a href={link} target="_blank" rel={"Visit my " + link_text} className='SocialLink'>
+            <img src={logo_src} alt={logo_alt} />
+            {link_text}
+        </a>
     )
 }
 
