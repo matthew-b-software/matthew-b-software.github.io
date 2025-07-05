670044bdf5e715a6efbbaafce9a85a3599180449
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-05 15:40:30 -0700

    Changed from import to link to maybe fix build

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,26 +1,23 @@
 import './ContactInfo.css'
 import Link from './Link'
-import LinkedInLogo from "/src/assets/LinkedIn_Logo.png"
-import GitHubLogo from "/src/assets/GitHub_Logo.png"
-import EmailLogo from "/src/assets/Email_Logo.png"
 
 const links = [
     {
         link: "https://www.linkedin.com/in/matthew-beitler/",
         link_text: "LinkedIn",
-        logo_src: LinkedInLogo,
+        logo_src: "/src/assets/LinkedIn_Logo.png",
         logo_alt: "LinkedIn Logo",
     },
     {
         link: "https://github.com/matthew-b-software",
         link_text: "GitHub",
-        logo_src: GitHubLogo,
+        logo_src: "/src/assets/GitHub_Logo.png",
         logo_alt: "GitHub Logo",
     },
     {
         link: "mailto:matthewbsoftware@gmail.com",
         link_text: "matthewbsoftware@gmail.com",
-        logo_src: EmailLogo,
+        logo_src: "/src/assets/Email_Logo.png",
         logo_alt: "Email Logo",
     }
 ]
