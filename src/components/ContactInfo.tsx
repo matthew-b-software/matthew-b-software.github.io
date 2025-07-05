6c28316e1aba9571b7ebf6d5942451a21a496fba
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-05 13:58:23 -0700

    Logo import working. Styling next

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,18 +1,13 @@
 import Link from './Link'
+import LinkedInLogo from "../assets/linkedin_logo.png"
 
 const links = [
     {
         link: "https://www.linkedin.com/in/matthew-beitler/",
         link_text: "LinkedIn",
-        logo_scr: "../assets/linkedin-logo.png",
+        logo_src: LinkedInLogo,
         logo_alt: "LinkedIn Logo",
     },
-    {
-        link: "https://www.linkedin.com/in/matthew-beitler/",
-        link_text: "LinkedIn",
-        logo_scr: "../assets/linkedin-logo.png",
-        logo_alt: "LinkedIn Logo",
-    }
 ]
 
 /** This component shall contain my contact info (links, email, & phone) */
@@ -26,7 +21,7 @@ function ContactInfo() {
                 <Link 
                     link={item.link}
                     link_text={item.link_text}
-                    logo_scr={item.logo_scr}
+                    logo_src={item.logo_src}
                     logo_alt={item.logo_alt}
                     />
                 </li>
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,16 +3,16 @@ import './Link.css'
 interface Props {
     link: string;
     link_text: string;
-    logo_scr: string;
+    logo_src: string;
     logo_alt: string;
 }
 
 /** This component shall contain a link and image to be used in "ContactInfo" */
-function Link({ link, link_text, logo_scr, logo_alt }: Props) {
+function Link({ link, link_text, logo_src, logo_alt }: Props) {
     return (
         <>
             <a href={link} target="_blank" rel={"Visit my " + link_text} className="SocialLink">
-                <img src={logo_scr} alt={logo_alt} />
+                <img src={logo_src} alt={logo_alt} />
                 {link_text}
             </a>
         </>
