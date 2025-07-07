12e7793acc20ed4d81a29c9207d893197e28a7cb
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-07 12:35:49 -0700

    Added test timeline data, started timeline tsx

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,12 @@
+
+
+/** This component shall contain a timeline of my career */
+function Timeline() {
+    return (
+        <>
+            <h1>HI</h1>
+        </>
+    )
+}
+
+export default Timeline
---
+++ src/data/Timeline.ts
@@ -0,0 +1,18 @@
+const TimelineData = [
+    {
+        title: "Software Engineer, LinkedIn",
+        date: "07/2025",
+        description: "Developed scalable AI systems and led a team of 5 engineers.",
+        icon_src: "/src/assets/LinkedIn_Logo.png",
+        icon_alt: "LinkedIn",
+    },
+    {
+        title: "Software Engineer, LinkedIn",
+        date: "04/2020",
+        description: "Developed scalable AI systems and led a team of 5 engineers.",
+        icon_src: "/src/assets/LinkedIn_Logo.png",
+        icon_alt: "LinkedIn",
+    }
+]
+
+export default TimelineData;
