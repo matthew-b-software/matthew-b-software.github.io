bebc964ecabc58edc82fa8694d7280a012a30865
matthew-b-software <matthewbsoftware@gmail.com>
2025-07-07 19:25:21 -0700

    Filled out *most* data in career timeline. Added logos for school and work

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -12,17 +12,19 @@ function Timeline() {
         <>
             <h1>Career</h1>
 
-            {TimelineData.map((item, index) => (
-            <TimelineItem 
-                key={index}
-                title={item.title} 
-                date={item.date}
-                description={item.description} 
-                icon_src={item.icon_src}
-                icon_alt={item.icon_alt}
-                selected={index === Selected} 
-                onClick={ () => setSelected(Selected === index ? -1 : index)} />
-            ))}
+            <div id="Timeline">
+                {TimelineData.map((item, index) => (
+                <TimelineItem 
+                    key={index}
+                    title={item.title} 
+                    date={item.date}
+                    description={item.description} 
+                    icon_src={item.icon_src}
+                    icon_alt={item.icon_alt}
+                    selected={index === Selected} 
+                    onClick={ () => setSelected(Selected === index ? -1 : index)} />
+                ))}
+            </div>
         </>
     )
 }
