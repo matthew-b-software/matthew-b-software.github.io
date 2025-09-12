8160a27114ac2de416be38a2f2913cc1c66c669b
matthew-b-software <matthewbsoftware@gmail.com>
2025-09-11 20:54:03 -0700

    Updated ContactInfo style

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,4 +1,4 @@
-import './ContactInfo.css'
+import { css } from '@emotion/react'
 import Link from './Link'
 import Links from '../data/ContactInfo.ts'
 
@@ -7,7 +7,7 @@ function ContactInfo() {
     return (
         <>
             <h1>Connect with Me</h1>
-            <ul id='ContactList'>
+            <ul css={ListStyle}>
                 {Links.map((item, index) => (
                 <li key={index}>
                 <Link 
@@ -22,5 +22,9 @@ function ContactInfo() {
         </>
     )
 }
+
+const ListStyle = css({
+    listStyle: 'none',
+})
 
 export default ContactInfo
