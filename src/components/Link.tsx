350c7ab384c42117b911ef27697a31cf573c3789
matthew-b-software <matthewbsoftware@gmail.com>
2025-09-11 20:26:38 -0700

    Link.tsx emotion styling working. Installed @emotion/css

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,4 +1,44 @@
-import './Link.css'
+import { css } from '@emotion/react'
+/* import './Link.css' */
+
+const SocialLink = css({
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    flexWrap: 'wrap',
+    color: 'var(--custom-black)',
+
+    fontSize: '1.5em',
+
+    ':hover': {
+        textDecoration: 'underline',
+        color: 'var(--custom-green)'
+    }
+})
+
+const SocialLinkImage = css({
+    marginRight: '15px',
+
+    display: 'flex',
+    alignItems: 'center',
+
+    height: 'auto',
+    width: 'auto',
+    maxHeight: '2.5em',
+    maxWidth: '100%',
+})
+
+const SocialLinkAnchor = css({
+    textDecoration: 'none',
+    fontSize: '1.5em',
+
+    ':hover': {
+        textDecoration: 'underline',
+        color: 'var(--custom-green)'
+    }
+})
+
+
 
 interface Props {
     link: string;
@@ -10,8 +50,8 @@ interface Props {
 /** This component shall contain a link and image to be used in "ContactInfo" */
 function Link({ link, link_text, logo_src, logo_alt }: Props) {
     return (
-        <a href={link} target="_blank" rel={"Visit my " + link_text} className='SocialLink'>
-            <img src={logo_src} alt={logo_alt} />
+        <a css={SocialLink} href={link} target="_blank" rel={"Visit my " + link_text}>
+            <img css={SocialLinkImage} src={logo_src} alt={logo_alt} />
             {link_text}
         </a>
     )
