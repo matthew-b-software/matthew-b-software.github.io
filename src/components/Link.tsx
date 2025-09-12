5acb96869b77b0a88a862d722091dfb953c4af48
matthew-b-software <matthewbsoftware@gmail.com>
2025-09-11 20:36:31 -0700

    Cleaned up link css/file

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,29 @@
 import { css } from '@emotion/react'
-/* import './Link.css' */
+
+interface Props {
+    link: string;
+    link_text: string;
+    logo_src: string;
+    logo_alt: string;
+}
+
+/** This component shall contain a link and image to be used in "ContactInfo" */
+function Link({ link, link_text, logo_src, logo_alt }: Props) {
+    return (
+        <a css={SocialLink} href={link} target="_blank" rel={"Visit my " + link_text}>
+            <img css={SocialLinkImage} src={logo_src} alt={logo_alt} />
+            {link_text}
+        </a>
+    )
+}
 
 const SocialLink = css({
     display: 'flex',
     alignItems: 'center',
     textDecoration: 'none',
     flexWrap: 'wrap',
+    
     color: 'var(--custom-black)',
-
     fontSize: '1.5em',
 
     ':hover': {
@@ -27,34 +43,5 @@ const SocialLinkImage = css({
     maxHeight: '2.5em',
     maxWidth: '100%',
 })
-
-const SocialLinkAnchor = css({
-    textDecoration: 'none',
-    fontSize: '1.5em',
-
-    ':hover': {
-        textDecoration: 'underline',
-        color: 'var(--custom-green)'
-    }
-})
-
-
-
-interface Props {
-    link: string;
-    link_text: string;
-    logo_src: string;
-    logo_alt: string;
-}
-
-/** This component shall contain a link and image to be used in "ContactInfo" */
-function Link({ link, link_text, logo_src, logo_alt }: Props) {
-    return (
-        <a css={SocialLink} href={link} target="_blank" rel={"Visit my " + link_text}>
-            <img css={SocialLinkImage} src={logo_src} alt={logo_alt} />
-            {link_text}
-        </a>
-    )
-}
 
 export default Link
