675bd9335e7ce0b5a5f111cd20c5f8c024915f4a
matthew-b-software <matthewbsoftware@gmail.com>
2025-09-24 14:42:20 -0700

    Moved css into tsx file, changed header text

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import { css } from '@emotion/react'
 import ContactInfo from './components/ContactInfo/ContactInfo'
 import ProjectGrid from './components/ProjectGrid/ProjectGrid'
 import Timeline from './components/Timeline/Timeline'
@@ -7,13 +7,40 @@ import Timeline from './components/Timeline/Timeline'
 function App() {
 
   return (
-    <div id='MainContainer'>
-      <h1> website development is in progress </h1>
+    <div css={AppStyle}>
+      <h1 css={AppHeader}> website development in progress </h1>
       <Timeline />
       <ProjectGrid />
       <ContactInfo />
     </div>
   )
 }
+
+const AppStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  minHeight: '100vh',
+  marginLeft: 'clamp(1rem, 4rem, 5rem)',
+  marginRight: 'clamp(1rem, 4rem, 5rem)',
+  marginTop: '0',
+  marginBottom: '16rem',
+
+  backgroundColor: 'var(--custom-white)',
+})
+
+const AppHeader = css({
+  fontSize: "3.2em",
+  lineHeight: '1.1',
+  color: 'var(--custom-black)',
+
+  /* underline to text */
+  borderBottom: '3px solid var(--custom-green)',
+  paddingBottom: '1rem',
+  marginBottom: '3rem',
+  width: '90%',
+})
 
 export default App
