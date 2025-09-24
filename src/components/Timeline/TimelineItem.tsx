f98d4b7ea745d5eb1592291eebe58edc4f6b4855
matthew-b-software <matthewbsoftware@gmail.com>
2025-09-23 19:43:37 -0700

    Moved CSS into tsx file

--- src/components/Timeline/TimelineItem.tsx
+++ src/components/Timeline/TimelineItem.tsx
@@ -1,4 +1,5 @@
-import "./TimelineItem.css"
+import { css } from '@emotion/react'
+// import "./TimelineItem.css"
 
 interface Props {
         title: string,
@@ -14,17 +15,54 @@ interface Props {
 function TimelineItem({ title, date, description, icon_src, icon_alt, selected, onClick }: Props) {
 
     return (
-        <div className="TimelineItem"> 
-            <div className="MainRow">
-                <img src={icon_src} alt={icon_alt}/>
-                <button className="TimelineButton" onClick={onClick}>
-                    <p>{date + ": " + title}</p>
-                    <p>{selected ? "\u2191": "\u2193"}</p>
+        <div css={TimelineItemStyle}> 
+            <div css={TimelineItemRow}>
+                <img css={TimelineItemImg} src={icon_src} alt={icon_alt}/>
+                <button css={TimelineItemButton} onClick={onClick}>
+                    <p css={TimelineItemText}>{date + ": " + title}</p>
+                    <p css={TimelineItemText}>{selected ? "\u2191": "\u2193" /* up arrow is unicode 2191 */ }</p> 
                 </button>
             </div>
             {selected && <div>{description}</div>}
         </div>
     )
 }
-/* up arrow is 2191 */
+
+const TimelineItemStyle = css({
+    fontSize: '1.5rem',
+    lineHeight: '1',
+    color: 'var(--custom-black)', 
+})
+
+const TimelineItemRow = css({
+    display: 'flex',
+    alignItems: 'center',
+})
+
+const TimelineItemImg = css({
+    height: 'auto',
+    width: 'auto',
+    maxHeight: '2.5em',
+    maxWidth: '100%',
+})
+
+const TimelineItemButton = css({
+    cursor: 'pointer',
+    border: 'none',
+    fontFamily: 'inherit',
+
+    display: 'flex',
+    alignItems: 'center',
+
+    ':hover': {
+        color: 'var(--custom-green)',
+    }
+})
+
+const TimelineItemText = css({
+    margin: '0',
+    marginLeft: '10px',
+    fontSize: '1.5rem',
+})
+
 export default TimelineItem
