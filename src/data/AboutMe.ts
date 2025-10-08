f937de31f7e4c5c529bb9371906b1a4bcb94ac61
matthew-b-software <matthewbsoftware@gmail.com>
2025-10-08 12:26:09 -0700

    added temp image data

---
+++ src/data/AboutMe.ts
@@ -0,0 +1,12 @@
+const links = [
+    {
+        img_path: "about_me/me_1.png",
+        img_alt: "a picture of me",
+    },
+    {
+        img_path: "about_me/me_2.png",
+        img_alt: "a picture of my dog",
+    }
+]
+
+export default links;
